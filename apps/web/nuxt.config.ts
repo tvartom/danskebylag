9153439cca1b7a68930886ce83b58eabdce9f23b
Lasse Hellman <github@tvartom.com>
2026-03-31 13:16:24 +0200

    No darkmode

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -48,6 +48,7 @@ export default defineNuxtConfig({
 			theme: {
 				preset: Aura,
 				options: {
+					darkModeSelector: false,
 					cssLayer: {
 						name: 'primevue',
 						order: 'theme, base, primevue',
